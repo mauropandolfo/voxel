b23e1caf9faba16540c3028fb95248f17ebe605c
Mauro Pandolfo <pandolfomauro@gmail.com>
2023-03-26 16:07:28 -0300

    primer commit - DisplayManager y DisplayRenderer

---
+++ src/voxel/MainGameLoop.java
@@ -0,0 +1,24 @@
+package voxel;
+
+import org.lwjgl.opengl.Display;
+
+import RenderEngine.DisplayManager;
+import RenderEngine.MasterRenderer;
+
+public class MainGameLoop {
+	
+	public static void main(String[] args) {
+		DisplayManager.createDisplay();
+		
+		MasterRenderer renderer = new MasterRenderer();
+		
+		while(!Display.isCloseRequested()) {
+			
+			renderer.prepare();
+			DisplayManager.updateDisplay();
+			
+		}
+		DisplayManager.closeDisplay();
+	}
+
+}
